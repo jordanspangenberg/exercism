a9873cab31f39d1874087717fa3fa66f55fc30c7
jordanspangenberg <jordan.spang@gmail.com>
2019-06-07 15:45:57 -0700

    add static typing to high_scores

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,12 +1,12 @@
 import heapq
-def latest(scores):
+def latest(scores: [int]) -> int:
     return scores[-1]
 
 
-def personal_best(scores):
+def personal_best(scores: [int]) -> int:
     return max(scores)
 
 
-def personal_top_three(scores):
+def personal_top_three(scores: [int]) -> [int]:
     heapq.heapify(scores)
     return heapq.nlargest(3, scores, key=None)
