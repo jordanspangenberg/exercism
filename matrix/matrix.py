9ae756dadbc962da4ad2a0eedc0e5b11d50c1935
jordanspangenberg <jordan.spang@gmail.com>
2019-06-07 19:18:48 -0700

    Complete matrix exercism

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,19 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.matrix = self.parseMatrix(matrix_string)
 
     def row(self, index):
-        pass
+        if index > len(self.matrix):
+            raise ValueError('Index Error: {index} is out of bounds')
+        return self.matrix[index-1]
 
     def column(self, index):
-        pass
+        if index > len(self.matrix[index-1]):
+            raise ValueError('Index Error: {index} is out of bounds')
+        return [m[index-1] for m in self.matrix]
+
+    def parseMatrix(self, str):
+        lines = str.split('\n')
+        return [[int(i) for i in s.split(' ')] for s in lines]
+
+
